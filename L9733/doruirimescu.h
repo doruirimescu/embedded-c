c349206780322edd47dce9a4823edcf3a89204b2
Doru Irimescu <doru.irimescu@sensible4.fi>
2021-09-30 11:55:54 +0300

    Add comments and use code style

--- L9733/doruirimescu.h
+++ L9733/doruirimescu.h
@@ -1,9 +1,16 @@
 /**
  *  @file doruirimescu.h
  *  @author Doru Irimescu
- *  @brief Driver interface for programming the L9733 integrated circuit for flight purposes.
+ *  @brief Driver interface for programming the L9733 integrated circuit for flight purposes
  *  I chose not to make unions with bitfields, because writing each bit individually makes it more clear in code.
- *  However, two macros is provided for clearing and setting the bitfields for convenience.
+ *  However, two macros are provided for clearing and setting the bitfields for convenience.
+ *  It is best to include the spi.h header in the source file, since it is only used there.
+ *
+ *  The naming convention which I have used: preffix _t for structure, suffix _e for enum. camelCase naming for functions.
+ *  For function parameters, suffix in_ if the parameter is an input, out_ if the parameter is an output and inout_ if
+ *  the parameter is both used as an input and output (not the case in here). In addition, the type of the parameter is
+ *  specified followed by the parameter name. Eg. in_ptParam denotes an input parameter which
+ *  is a pointer to structure.
  */
 
 #ifndef _DORUIRIMESCU_H_
@@ -101,27 +108,27 @@ typedef struct
 } fault_report_t;
 
 /**
- * @brief  Programs the chip with an output status configuration. Returns status.
+ * @brief  Programs the chip with an output status configuration. Returns fault report.
  *
- * @param  in_config : output status configuration
- * @param  out_status : fault status report
+ * @param  in_ptConfig : output status configuration
+ * @param  out_ptStatus : fault status report
  */
-void writeOutputStatusConfiguration(const output_status_t const* in_config, fault_report_t const* out_status);
+void writeOutputStatusConfiguration(const output_status_t const* in_ptConfig, fault_report_t const* out_ptStatus);
 
 /**
- * @brief  Programs the chip with a diagnostic mode configuration. Returns status.
+ * @brief  Programs the chip with a diagnostic mode configuration. Returns fault report.
  *
- * @param  in_config : diagnostic mode configuration
- * @param  out_status : fault status report
+ * @param  in_ptConfig : diagnostic mode configuration
+ * @param  out_ptStatus : fault status report
  */
-void writeDianosticModeConfiguration(const diag_mode_t const* in_config, fault_report_t const* out_status);
+void writeDianosticModeConfiguration(const diag_mode_t const* in_ptConfig, fault_report_t const* out_ptStatus);
 
 /**
- * @brief  Programs the chip with an overcurrent protection configuration. Returns status.
+ * @brief  Programs the chip with an overcurrent protection configuration. Returns fault report.
  *
- * @param  in_config : overcurrent protection configuration
- * @param  out_status : fault status report
+ * @param  in_ptConfig : overcurrent protection configuration
+ * @param  out_ptStatus : fault status report
  */
-void writeOvercurrentProtectionConfiguration(const overcurrent_protection_t const* in_config, fault_report_t const* out_status);
+void writeOvercurrentProtectionConfiguration(const overcurrent_protection_t const* in_ptConfig, fault_report_t const* out_ptStatus);
 
 #endif
